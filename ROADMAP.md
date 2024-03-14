565c516f8cb31ff7b621510a0dc1c51be44dacc9
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-03-13 21:46:31 -0700

    Conditionally render `/event` page
    Co-Authored-By: dzyfer <dzyfer@dzyfer.com>

--- ROADMAP.md
+++ ROADMAP.md
@@ -4,8 +4,8 @@
 - [x] Styles
 - [x] Authentication
 - [x] Countdown to event start
-- [ ] Configurable state of event: Vercel Edge Config
-- [ ] Conditional rendering of content based on state of event
+- [x] Configurable state of event: Vercel Edge Config
+- [x] Conditional rendering of content based on state of event
 - [ ] Registration: Stripe
 - [ ] Data layer: Vercel Postgres or Notion
 - [ ] Fetching riddle from Notion
--- app/event/page.tsx
+++ app/event/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link'
 import { auth } from '@/auth'
-import { checkAuth } from '@/functions/user-management'
+import { get } from '@vercel/edge-config'
 import { redirect } from 'next/navigation'
 import { Spacer } from '@/components/Spacer'
+import { checkAuth } from '@/functions/user-management'
+import { CountdownWrapper } from '@/components/CountdownWrapper'
 
 export default async function Event() {
   const session = await auth()
@@ -16,13 +19,49 @@ export default async function Event() {
       <h1 id='title'><code className="blackCode">cascade</code></h1>
       <Spacer size={32} />
       <p>
-        welcome {session.user?.name?.split(' ')[0].toLowerCase() || 'user'}, <br />
+        welcome {session.user?.name?.split(' ')[0].toLowerCase() || 'user'},
+      </p>
+      <Content />
+    </>
+  )
+}
+
+async function Content() {
+  const registered = true // TODO: implement with db + Stripe
+  const started = await get('eventStarted')
+
+  if (!registered)
+    return <Unregistered />
+  else if (registered && !started)
+    return <RegisteredAndWaiting />
+}
+
+function RegisteredAndWaiting() {
+  return (
+    <>
+      <p>you&apos;ve registered for compsigh <code>cascade</code></p>
+      <p>the event will begin in <code className="blackCode">
+          <CountdownWrapper
+            date={1712971800000}
+            autoStart={true}
+          />
+      </code></p>
+    </>
+  )
+}
+
+function Unregistered() {
+  return (
+    <>
+      <p>
         to register for &amp; participate in compsigh <code>cascade</code>, please deposit $10 below
       </p>
       <Spacer size={32} />
-      <p>
-        you&apos;ve registered for compsigh <code>cascade</code>. the event will begin in <code className="blackCode">12:01:46:10</code>
-      </p>
+      <ul>
+        <li>
+          <Link href="/register">register</Link>
+        </li>
+      </ul>
     </>
   )
 }
