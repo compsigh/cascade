77a3ad71f004aa6f7fafb9efd77e53f84dd11d2e
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-04-02 17:58:21 -0700

    Add accept/decline invite to `Button`

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,8 +4,12 @@ import { signIn } from 'next-auth/react'
 import styles from './Button.module.css'
 
 export function Button(
-  { type, text }:
-  { type: 'signIn' | 'stripe', text: string }
+  { type, text, inviteId }:
+  {
+    type: 'signIn' | 'stripe' | 'accept-invite' | 'decline-invite',
+    text: string,
+    inviteId?: string
+  }
 ) {
   if (type === 'signIn')
     return (
@@ -20,6 +24,28 @@ export function Button(
   if (type === 'stripe')
     return (
       <form action='/api/stripe' method='POST'>
+        <button type='submit' className={styles.button}>
+          {text}
+        </button>
+      </form>
+    )
+
+  if (type === 'accept-invite')
+    return (
+      <form action='/api/db/invites' method='POST'>
+        <input type='hidden' name='id' value={inviteId} />
+        <input type='hidden' name='action' value='accept' />
+        <button type='submit' className={styles.button}>
+          {text}
+        </button>
+      </form>
+    )
+
+  if (type === 'decline-invite')
+    return (
+      <form action='/api/db/invites' method='POST'>
+        <input type='hidden' name='id' value={inviteId} />
+        <input type='hidden' name='action' value='decline' />
         <button type='submit' className={styles.button}>
           {text}
         </button>
