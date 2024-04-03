b92fa7d1c96a109d340b83ba6ba1f6b0af5f1df9
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-04-02 18:53:24 -0700

    Remove invites from `Button` for Server Actions

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,19 +4,13 @@ import { signIn } from 'next-auth/react'
 import styles from './Button.module.css'
 
 export function Button(
-  { type, text, inviteId, fromEmail, toEmail }:
-  {
-    type: 'signIn' | 'stripe' | 'send-invite' | 'accept-invite' | 'decline-invite',
-    text: string,
-    inviteId?: string,
-    fromEmail?: string,
-    toEmail?: string
-  }
+  { type, text }:
+  { type: 'signIn' | 'stripe' | 'submit', text: string }
 ) {
   if (type === 'signIn')
     return (
       <button
-        onClick={() => signIn('google', { callbackUrl: '/event' })}
+        onClick={() => signIn("google", { callbackUrl: "/event" })}
         className={styles.button}
       >
         {text}
@@ -25,44 +19,18 @@ export function Button(
 
   if (type === 'stripe')
     return (
-      <form action='/api/stripe' method='POST'>
-        <button type='submit' className={styles.button}>
+      <form action="/api/stripe" method="POST">
+        <button type="submit" className={styles.button}>
           {text}
         </button>
       </form>
     )
 
-  if (type === 'send-invite')
+  if (type === 'submit')
     return (
-      <form action='/api/db/invite' method='POST'>
-        <input type='hidden' name='from' value={fromEmail} />
-        <input type='hidden' name='to' value={toEmail} />
-        <button type='submit' className={styles.button}>
-          {text}
-        </button>
-      </form>
-    )
-
-  if (type === 'accept-invite')
-    return (
-      <form action='/api/db/invite' method='PUT'>
-        <input type='hidden' name='id' value={inviteId} />
-        <input type='hidden' name='action' value='accept' />
-        <button type='submit' className={styles.button}>
-          {text}
-        </button>
-      </form>
-    )
-
-  if (type === 'decline-invite')
-    return (
-      <form action='/api/db/invite' method='PUT'>
-        <input type='hidden' name='id' value={inviteId} />
-        <input type='hidden' name='action' value='decline' />
-        <button type='submit' className={styles.button}>
-          {text}
-        </button>
-      </form>
+      <button type="submit" className={styles.button}>
+        {text}
+      </button>
     )
 
   return <></>
