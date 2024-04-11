f17f5a6b9b7dc5377da4248caa8839937f232d2e
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-04-11 13:37:27 -0700

    Fix `Countdown` hydration error

--- components/CountdownWrapper/CountdownWrapper.tsx
+++ components/CountdownWrapper/CountdownWrapper.tsx
@@ -2,17 +2,29 @@
 
 import Countdown from 'react-countdown'
 import type { ReactElement } from 'react'
+import { useState, useEffect } from 'react'
 
 export function CountdownWrapper(
   { date, autoStart, revealedOnCompletion }:
   { date: number, autoStart: boolean, revealedOnCompletion?: ReactElement}
 ) {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    setIsLoaded(true)
+  }, [])
+
+  if (!isLoaded)
+    return <span>00:00:30:00</span>
+
   return (
-    <Countdown
-      date={date}
-      autoStart={autoStart}
-    >
-      {revealedOnCompletion}
-    </Countdown>
+    <>
+      <Countdown
+        date={date}
+        autoStart={autoStart}
+      >
+        {revealedOnCompletion}
+      </Countdown>
+    </>
   )
 }
