dd5056971843fbc0b4a1f8a950c42bb48b9e7563
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-10-16 12:42:50 -0700

    Compose answer `Input`

---
+++ components/Input/Input.tsx
@@ -0,0 +1,18 @@
+import { validateInputServerAction } from '@/functions/db'
+
+import { Spacer } from '@/components/Spacer'
+import { Button } from '@/components/Button'
+
+export function Input({ teamId, part }: { teamId: string, part: number }) {
+  return (
+    <>
+      <form action={validateInputServerAction}>
+        <input type="hidden" name="teamId" value={teamId} />
+        <input type="hidden" name="part" value={part.toString()} />
+        <input type="text" name="answer" placeholder="answer" />
+        <Spacer size={8} />
+        <Button type="submit" text="submit" />
+      </form>
+    </>
+  )
+}
