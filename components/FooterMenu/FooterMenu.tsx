da36cc61f2ba1ce0a2c94b9aeea5198c19a799c3
nishoof <88177986+nishoof@users.noreply.github.com>
2025-04-15 16:00:17 -0700

    Remove sign in, team page, riddles page

--- components/FooterMenu/FooterMenu.tsx
+++ components/FooterMenu/FooterMenu.tsx
@@ -1,17 +1,8 @@
 "use client";
 
 import styles from "./FooterMenu.module.css";
-import { signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-function getSignInItem(signedIn: boolean) {
-  return (
-    signedIn ?
-      <Link href="/" onClick={() => signOut()}>sign out</Link>
-      : <Link href="/" onClick={() => signIn('google', { callbackUrl: '/' })}>sign in</Link>
-  );
-}
 
 function getLinkItem(text: string, href: string, currPath: string) {
   const style = href === currPath ? "selectedText" : "";
@@ -22,18 +13,13 @@ function getLinkItem(text: string, href: string, currPath: string) {
   );
 }
 
-export function FooterMenu(
-  { signedIn }: { signedIn: boolean }
-) {
+export function FooterMenu() {
   const currentPath = usePathname();
 
   return (
     <nav className={styles.footer}>
       {getLinkItem("cascade", "/", currentPath)}
       {getLinkItem("details", "/details", currentPath)}
-      {signedIn && getLinkItem("team", "/team", currentPath)}
-      {signedIn && getLinkItem("riddles", "/riddles", currentPath)}
-      {getSignInItem(signedIn)}
     </nav>
   );
 }
