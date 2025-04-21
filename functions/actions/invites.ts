6aa99277c6f19562c1c2aaddf298bcd30febf3c7
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-04-21 02:45:46 -0700

    Revert "Remove redundant `sendInviteServerAction()`"
    This reverts commit dc47332e459f8bf2530775f9b48a636535dd3658.

--- functions/actions/invites.ts
+++ functions/actions/invites.ts
@@ -3,14 +3,10 @@
 import { revalidatePath } from "next/cache"
 import { sendInvite, cancelInvite } from "@/functions/db/invites"
 
-export async function sendInviteServerAction(formData: FormData) {
-  const rawFormData = {
-    from: formData.get("from") as string,
-    to: formData.get("to") as string
-  }
-
-  await sendInvite(rawFormData.from, rawFormData.to)
+export async function sendInviteServerAction(from: string, to: string) {
+  const invite = await sendInvite(from, to)
   revalidatePath("/event")
+  return invite
 }
 
 export async function cancelInviteServerAction(id: string) {
